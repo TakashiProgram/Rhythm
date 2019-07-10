ab66edc23389d51af9bd2ce0fb959e9620f12db6
1401341souama <uaebnncdeidie@gmail.com>
2019-07-10 22:58:44 +0900

    タップしたらエフェクトが出るようにした シーンを切り替えられるようにした

--- Rhythm/Assets/MyPakage/Scripts/Title/TitleManager.cs
+++ Rhythm/Assets/MyPakage/Scripts/Title/TitleManager.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class TitleManager : MonoBehaviour {
     [SerializeField]
     private RayCast m_Raycast;
+    [SerializeField]
+    private EffectManager m_EffectManager;
+
+    [SerializeField]
+    private LoadScene m_LoadScene;
+
     void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButton(0)){
-            GameObject obj = m_Raycast.RayerHitObject();
-            Debug.Log(obj);
-        }
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject obj = m_Raycast.RayerHitObject();
+            LoadScene load_scene = obj.GetComponent<LoadScene>();
+            if (null != load_scene)
+            {
+                load_scene.ChangeScene();
+            }
+
+            Vector3 tap = Input.mousePosition;
+            tap.z = 8;
+            m_EffectManager.TapEffect(Camera.main.ScreenToWorldPoint(tap));
+        }
     }
 }
